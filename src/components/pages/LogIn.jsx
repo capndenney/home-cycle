f39bc738a0014e9fbcff7c9afd8cb14c41372a9f
Mike Denney <mike.a.denney@gmail.com>
2025-11-05 18:19:50 -0600

    created button component

--- src/components/pages/LogIn.jsx
+++ src/components/pages/LogIn.jsx
@@ -1,6 +1,13 @@
+import Input from "../common/Input"
+
 const LogIn = () => {
     return (
-        console.log("LogIn")
+        <div>
+            <h2>Log In</h2>
+            <Input /> {/* TODO: Add details to point to Username */}
+            <Input /> {/* TODO: Add details to point to Password */}
+            <
+        </div>
     )
 }
 
