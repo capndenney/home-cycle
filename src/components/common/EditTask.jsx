7963d223c5e7f77c494f687bc576cbcd1805daee
Mike Denney <mike.a.denney@gmail.com>
2025-11-05 19:24:52 -0600

    created edit task skeleton from existing card component

--- src/components/common/EditTask.jsx
+++ src/components/common/EditTask.jsx
@@ -1,8 +1,20 @@
 import Card from "./Card"
+import Input from "./Input"
+import Button from "./Button"
+import { DayPicker } from "react-day-picker"
 
 const EditTask = () => {
     return (
-        console.log("EditTask")
+        <Card>
+          <h3>TODO: Prop for Task Name</h3>
+          <p>TODO: Load Task Number</p>
+          <Input>TODO: Task Title</Input>
+          <Input>TODO: Task Description</Input>
+          <Input>TODO: Completed Checkbox</Input>
+          <DayPicker>TODO: Load Saved Date</DayPicker>
+          <Button>TODO: Save Changes</Button>
+          <Button>TODO: Cancel Changes</Button>
+        </Card>
     )
 }
 
