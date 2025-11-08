51141250f5433367341ab52f9bc24fb53871b929
Mike Denney <mike.a.denney@gmail.com>
2025-11-08 16:16:23 -0600

    card spacing added, fixed checkerboarding

--- src/components/common/Card.jsx
+++ src/components/common/Card.jsx
@@ -1,8 +1,12 @@
 import "./card.css";
+import Spacer from "./Spacer";
 
 const Card = ({ viewType, children }) => {
     return (
+        <>
         <div className={`card ${viewType}`}>{children}</div>
+        {/* <Spacer /> */}
+        </>
     )
 }
 
