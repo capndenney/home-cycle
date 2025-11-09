db94bed7c65b39eb5086d2cca440fa2a7dbf809d
Mike Denney <mike.a.denney@gmail.com>
2025-11-08 19:18:11 -0600

    added logo asset

--- src/components/common/TaskCard.jsx
+++ src/components/common/TaskCard.jsx
@@ -2,14 +2,15 @@ import Card from "./Card"
 import Button from "./Button"
 
 const TaskCard = ({ title, id, description, dueDate }) => {
+
     return (
         <Card viewType="view">
           <h3>{title}</h3>
           <p>{id}</p>
           <p>{description}</p>
-          <p>{dueDate}</p>
+          <p>Due Date: {dueDate}</p>
           <p>TODO: Learning Content?</p>
-          <Button id={`task-${id}`}>TODO: Edit Changes</Button>
+          <Button id={`task-${id}`} label="Edit" />
         </Card>
     )
 }
