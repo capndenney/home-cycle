a853a3da66f7cbf5f13f29d38528ff460dca644e
Mike Denney <mike.a.denney@gmail.com>
2025-11-15 11:29:59 -0600

    task card loading, need final styling

--- src/components/common/EditTask.jsx
+++ src/components/common/EditTask.jsx
@@ -3,14 +3,14 @@ import Input from "./Input"
 import Button from "./Button"
 import { DayPicker } from "react-day-picker"
 
-const EditTask = ( ) => {
+const EditTask = ( task ) => {
     return (
         <Card viewType="edit add-blur">
-          <h3>TODO: Task Name</h3>
-          <p>TODO: TaskID</p>
-          <Input label="TODO: taskName"/>
-          <Input label="TODO: taskDesc"/>
-          <Input label="TODO: completed"/>
+          <h3>{task.title}</h3>
+          <p>{task.id}</p>
+          <Input label={task.title}/>
+          <Input label={task.description}/>
+          <Input label={task.completed}/>
           <DayPicker mode="single">TODO: Load Saved Date</DayPicker>
           <Button label="Save">TODO: Save Changes</Button>
           <Button label="Cancel">TODO: Cancel Changes</Button>
--- src/components/common/TaskCard.jsx
+++ src/components/common/TaskCard.jsx
@@ -1,18 +1,32 @@
-import Card from "./Card"
-import Button from "./Button"
+import Card from "./Card";
+import Button from "./Button";
+import { useParams } from "react-router";
+import { useState, useEffect } from "react";
+import sampleTasks from "../../sampleData/sampleTasks";
 
-const TaskCard = ({ title, id, description, dueDate }) => {
+const TaskCard = ( ) => {
+  const { id } = useParams();
+  const [task, setTask] = useState(null);
 
+  useEffect(() => {
+    const foundTask = sampleTasks.find((t) => t.taskId === Number(id));
+    setTask(foundTask);
+  }, [id]);
+
+  if (task) {
     return (
-        <Card viewType="view">
-          <h3>{title}</h3>
-          <p>{id}</p>
-          <p>{description}</p>
-          <p>Due Date: {dueDate}</p>
-          <p>TODO: Learning Content?</p>
-          <Button id={`task-${id}`} label="Edit" />
-        </Card>
-    )
-}
+      <Card viewType="view">
+        <h3>{task.title}</h3>
+        <p>{task.taskId}</p>
+        <p>{task.description}</p>
+        <p>Due Date: {task.dueDate}</p>
+        <p>TODO: Learning Content?</p>
+        <Button id={`task-${task.taskId}`} label="Edit" />
+      </Card>
+    );
+  } else {
+    return <h3>Task not Found!</h3>;
+    }
+};
 
-export default TaskCard
+export default TaskCard;
