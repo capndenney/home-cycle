af6deb5d92c8a427a9cfef7bcc8d0a1dd64324de
Mike Denney <mike.a.denney@gmail.com>
2025-11-16 10:11:43 -0600

    added state to title so it updates preview as it is edited

--- src/components/common/EditTask.jsx
+++ src/components/common/EditTask.jsx
@@ -3,26 +3,43 @@ import Input from "./Input"
 import Button from "./Button"
 import { DayPicker } from "react-day-picker"
 import { useState } from "react"
+import sampleTasks from "../../sampleData/sampleTasks"
 
 const EditTask = ( task ) => {
 
-  const {formData, setFormData} = useState()
-  const handleChange = (e) => {
-    setFormData({...formData, [e.target.name]: e.target.value})
+  const newTaskId = sampleTasks.reduce((max, t) => { //TODO: Use newTaskId for new task, load taskId for existing tasks
+    return t.taskId > max ? t.taskId : max;
+  }, 0) + 1; 
+
+  const initialFeedback = { name: "", email: "", feedback: "" }; 
+  const [inputData, setInputData] = useState(initialFeedback);
+  const [titleData, setTitleData] = useState(null)
+
+  const titleChange = (e) => {
+    setTitleData(e.target.value)
   }
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setInputData((oldData) => ({ ...oldData, [id]: value }));
+  };
+
+  // const {formData, setFormData} = useState()
+  // const handleChange = (e) => {
+  //   setFormData({...formData, [e.target.name]: e.target.value})
+  // }
 
   const handleSave = (e) => {
     e.preventDefault();
-
   }
 
     return (
         <Card viewType="edit add-blur">
-          <h3>{task.title}</h3>
-          <p>{task.id}</p>`
-          <Input label={task.title} id={`input-title-${task.id}`}/>
-          <Input label={task.description}/>
-          <Input label={task.completed}/>
+          <h3>{titleData}</h3>
+          <p>{task.id ? task.id : newTaskId}</p>
+          <Input label={task.title} id={`input-title-${task.id}`} handleChange={titleChange}/>
+          <Input label={task.description} handleChange={handleChange}/>
+          <Input type="checkbox" label={task.completed}/>
           <DayPicker mode="single">TODO: Load Saved Date</DayPicker>
           <Button label="Save">TODO: Save Changes</Button>
           <Button label="Cancel">TODO: Cancel Changes</Button>
